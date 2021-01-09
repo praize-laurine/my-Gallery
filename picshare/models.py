3f395bd8547cc9bc4fc69e4f17077834c85a1e53
praise-laurine <praiselaurine10@gmail.com>
2021-01-10 01:09:38 +0300

    added location and category to image model

--- picshare/models.py
+++ picshare/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model):
+    # image = models.ImageField(upload_to = 'images/', default = 'image.jpeg')
     name = models.CharField(max_length = 30)
     description = models.TextField()
+    location = models.ForeignKey('Location')
+    category = models.ForeignKey('Category')
 
 
 class Category(models.Model):

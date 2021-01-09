42f493a2bf52a2219c22ed9e3d5d6f873f5b92c9
praise-laurine <praiselaurine10@gmail.com>
2021-01-10 01:32:17 +0300

    added a testcase for images

--- picshare/tests.py
+++ picshare/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class ImagesTestCase(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.location = Location(name = 'Nakuru')
+        self.location.save()
+
+        self.category = Category(name = 'Adventure')
+        self.category.save()
+
+        self.new_image = Images(name = 'image', description = 'this is a nice image', location = self.location, category = self.category)
+        self.new_image.save()
+
+    def tearDown(self):
+        Images.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()    
+
+

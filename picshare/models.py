29b1de59ed1d7c16650c742b7e024b076c288070
praise-laurine <praiselaurine10@gmail.com>
2021-01-10 02:45:09 +0300

    installed pillow

--- picshare/models.py
+++ picshare/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model):
-    # image = models.ImageField(upload_to = 'images/', default = 'image.jpeg')
+    image = models.ImageField(upload_to = 'images/', default = 'image.jpeg')
     name = models.CharField(max_length = 30)
     description = models.TextField()
-    location = models.ForeignKey('Location')
-    category = models.ForeignKey('Category')
+    location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
 
     def save_image(self):
         self.save()

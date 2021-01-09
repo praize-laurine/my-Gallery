b811fd65338a8451ebaaf978c0c7f1b7d2860897
praise-laurine <praiselaurine10@gmail.com>
2021-01-10 02:51:29 +0300

    defined function for images

--- picshare/views.py
+++ picshare/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def images(request):
+    image = Images.objects.all()
+    return render(request, 'image.html', {'image' : image})    

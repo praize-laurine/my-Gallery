5f28cba7d56f9c7cf765e288cdfab957a87993f2
praise-laurine <praiselaurine10@gmail.com>
2021-01-13 08:08:22 +0300

    deployment to heroku

--- pictures/settings.py
+++ pictures/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import cloudinary
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
-import cloudinary
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

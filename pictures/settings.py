e6924bb463656e6e76fa1bd1aac2833cb8dfee12
praise-laurine <praiselaurine10@gmail.com>
2021-01-13 08:53:06 +0300

    deployment to heroku

--- pictures/settings.py
+++ pictures/settings.py
@@ -88,10 +88,10 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
+           'NAME':'gallery',
+           'USER':'moringa',
+           'PASSWORD':'1234',
+           'HOST':('praisegallery.herokuapp.com'),
            'PORT': '',
        }
        
